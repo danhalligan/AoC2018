f4f8b6c1d37c421d37133d10dca41c232413c8c2
Daniel Halligan <dan.halligan@gmail.com>
2022-12-04 17:37:37 +0000

    Finalise day15

--- aoc2018/day15.py
+++ aoc2018/day15.py
@@ -1,49 +1,17 @@
 import aoc2018.goblins
-import importlib
-importlib.reload(aoc2018.goblins)
-
-x = [list(x) for x in open("tests/inputs/day15.txt").read().splitlines()]
-cave = aoc2018.goblins.Cave(x)
-# cave.print()
-u = cave.units[0]
-u.targets(cave)
-u.target_open(cave)
-u.destination(cave)
-u.position = u.move(cave)
-
-# cave.shortest_paths((1,1), (3,3))
-
-paths = cave.dij((1,1))
-end = (3,3)
 
 
-
-list(recurse([], (2,3)))
-
-
-for p in target_open(cave):
-    
-unit = x.units[0]
-x.destination(unit)
-
-cave.distance((1,1))
-
-others = [u for u in x.units if unit != u]
-taken = [u.position for u in others]
-avail = [x.neighbours(u.position, taken) for u in others]
-avail = sum(avail, [])
-dist = [x.distance(p, taken).get(unit.position, 1000) for p in avail]
-pos = [a for d, a in zip(dist, avail) if d == min(dist)]
-dest = sorted(pos)[0]
+def part1():
+    cave = aoc2018.goblins.Cave("inputs/day15.txt")
+    done = False
+    while not done:
+        done = cave.round(verbose=False)
+    return str(cave.score())
 
 
-for u in x.units:
-    if unit != u:
-        breakpoint()
-        x.distance(u.position, taken)[unit.position]
-
-x.distance(x.units[1].position, taken)[unit.position]
-
-
-def round()
-    for unit in sort(units, key = lambda x: x.pos):
+def part2():
+    cave = aoc2018.goblins.Cave("inputs/day15.txt", elf_attack=13)
+    done = False
+    while not done:
+        done = cave.round2(verbose=False)
+    return str(cave.score())
--- aoc2018/goblins.py
+++ aoc2018/goblins.py
@@ -1,4 +1,5 @@
 from heapq import *
+from termcolor import colored
 
 # store position as (column, row)
 class Unit:
@@ -15,11 +16,11 @@ class Unit:
 
     def open(self, cave):
         """Find open neighbouring positions (avoids units)"""
-        return [p for p in self.neighbours(cave) if p not in cave.positions()]
+        return [p for p in self.neighbours(cave) if p not in cave.positions_alive()]
 
     def targets(self, cave):
         """Find all targets"""
-        return [u for u in cave.units if type(u) != type(self)]
+        return [u for u in cave.units if type(u) != type(self) and u.alive()]
 
     def target_open(self, cave):
         """Find open locations adjacent to all targets"""
@@ -27,111 +28,143 @@ class Unit:
 
     def any_targets(self, cave):
         """Any targets left?"""
-        return len(self.targets(cave) > 0)
+        return len(self.targets(cave)) > 0
 
     def in_range(self, cave):
-        """Are we in range of a target?"""
-        return any(self.position in u.neighbours(cave) for u in self.targets(cave))
+        """List of in range targets"""
+        return [u for u in self.targets(cave) if self.position in u.neighbours(cave)]
 
-    def destination(self, cave):
-        """Choose open target to move to"""
-        dists = cave.distance(self.position)
-        dests = self.target_open(cave)
-        dists = {k: v for k, v in dists.items() if k in dests}
-        min_dist = min(dists.values())
-        return sorted([k for k, v in dists.items() if v == min_dist])[0]
+    def alive(self):
+        return self.hit_points > 0
 
-    def move(self, cave):
+    def move(self, cave, verbose=False):
         """Move unit closer to nearest target along shortest path"""
-        dest = self.destination(cave)
-        paths = cave.shortest_paths(self.position, dest)
+        dests = self.target_open(cave)
+        paths = cave.dij(self.position, dests)
+        dists = {p: x["s"] for p, x in paths.items()}
+        dists = {k: v for k, v in dists.items() if k in dests}
+        if not len(dists):
+            return None
+        min_dist = min(dists.values())
+        dest = sorted([k for k, v in dists.items() if v == min_dist])[0]
+
+        paths = cave.shortest_paths(self.position, dest, paths)
         moves = [x[0] for x in paths]
-        u.position = sorted(moves)[0]
+        if verbose:
+            print(f"Moving {self} to {sorted(moves)[0]}")
+        self.position = sorted(moves)[0]
 
-    def attack(self):
+    def attack(self, cave):
         # find all targets in range
-        # find target with fewest hit points
+        avail = self.in_range(cave)
+
+        # find target with fewest hit points (breaking ties as required)
+        hp = min(x.hit_points for x in avail)
+        avail = [x for x in avail if x.hit_points == hp]
+        target = sorted(avail, key=lambda x: x.position)[0]
+
         # deal damage
-        return None
+        target.hit_points -= self.attack_power
 
-    def play(self):
-        # identify targets
-        # identify open squares adjacent to each target
-        # if not in range and no open square in range: end
-        # if in range -- attack
-        # else -- move
-        return None
-
-    def code(self):
-        return type(self).__name__[0]
+    def play(self, cave, verbose=False):
+        if verbose:
+            print(f"Targets: {self.targets(cave)}")
+        if not self.any_targets(cave):
+            return False
+        if verbose:
+            print(f"In range: {self.in_range(cave)}")
+        if not len(self.in_range(cave)):
+            self.move(cave, verbose=verbose)
+        if len(self.in_range(cave)):
+            self.attack(cave)
+        return True
 
     def __repr__(self):
-        return f"{self.code()}{self.position}"
+        return f"{self.code()}{self.position}:{self.hit_points}"
 
 
 class Goblin(Unit):
+    def code(self):
+        return colored("G", "green")
+
     pass
 
 
 class Elf(Unit):
+    def __init__(self, position, attack_power=3):
+        self.hit_points = 200
+        self.attack_power = attack_power
+        self.position = position
+
+    def code(self):
+        return colored("E", "red")
+
     pass
 
 
 class Cave:
-    def __init__(self, array):
+    def __init__(self, file, elf_attack=3):
+        array = [list(x) for x in open(file).read().splitlines()]
         """Create a new cave from input as an array"""
         self.units = []
         self.map = {}
         self.rows = len(array)
         self.cols = len(array[0])
+        self.round_count = 0
         for r in range(len(array)):
             for c in range(len(array[r])):
                 if array[r][c] == "G":
                     self.units.append(Goblin((r, c)))
                     self.map[(r, c)] = "."
                 elif array[r][c] == "E":
-                    self.units.append(Elf((r, c)))
+                    self.units.append(Elf((r, c), attack_power=elf_attack))
                     self.map[(r, c)] = "."
                 else:
                     self.map[(r, c)] = array[r][c]
 
-    def open_positions(self):
+    def positions(self):
+        return [u.position for u in self.units]
+
+    def positions_alive(self):
+        return [u.position for u in self.units if u.alive()]
+
+    def positions_open(self):
         """All open positions in cave"""
         return [
             (r, c)
             for r in range(self.rows)
             for c in range(self.cols)
-            if self.map[r, c] == "." and (r, c) not in self.positions()
+            if self.map[r, c] == "." and (r, c) not in self.positions_alive()
         ]
 
     def neighbours(self, pos):
         """Open neighbours in cave"""
         i, j = pos
         coords = [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]
-        return list(set(coords) & set(self.open_positions()))
+        return list(set(coords) & set(self.positions_open()))
 
-    def dij(self, start):
+    def dij(self, start, dests):
         """Find all distance to every open position, retaining parent cells"""
         paths = {start: {"s": 0, "p": []}}
         queue = [(0, start)]
+        best = 100000
         while len(queue):
             score, pos = heappop(queue)
             for nb in self.neighbours(pos):
                 new = score + 1
+                if new > best:
+                    break
                 scores = {p: x["s"] for p, x in paths.items()}
                 if new < scores.get(nb, 100000):
                     paths[nb] = {"s": new, "p": [pos]}
                     heappush(queue, (new, nb))
                 elif new == scores[nb]:
                     paths[nb] = {"s": new, "p": paths[nb]["p"] + [pos]}
+                if nb in dests:
+                    best = min(best, new)
         return paths
 
-    def distance(self, pos):
-        """Find shortest distance to every open position"""
-        paths = self.dij(pos)
-        return {p: x["s"] for p, x in paths.items()}
-
-    def shortest_paths(self, start, end):
+    def shortest_paths(self, start, end, paths):
         """Find all shortest paths between two positions"""
 
         def recurse(path, pos):
@@ -141,31 +174,7 @@ class Cave:
             else:
                 yield path
 
-        paths = self.dij(start)
         return list(recurse([], end))
-
-    # Find the destination target.
-    # Targets defined as being a neighbouring square of an enemy unit
-    # We find the target with the minimum distance
-    # ties are broken by reading order
-    def destination(self, unit):
-        others = [u for u in self.units if unit != u]
-        taken = [u.position for u in others]
-        targets = [
-            self.neighbours(u.position, taken)
-            for u in others
-            if type(others) != type(unit)
-        ]
-        targets = sum(targets, [])
-        dist = [self.distance(p, taken).get(unit.position, 1000) for p in targets]
-        pos = [a for d, a in zip(dist, targets) if d == min(dist)]
-        return sorted(pos)[0]
-
-    def positions(self):
-        return [u.position for u in self.units]
-
-    def in_range(self, unit):
-        pass
 
     def print(self):
         for r in range(self.rows):
@@ -173,7 +182,47 @@ class Cave:
             for c in range(self.cols):
                 if (r, c) in self.positions():
                     u = self.units[self.positions().index((r, c))]
-                    s += u.code()
+                    if u.alive():
+                        s += u.code()
+                    else:
+                        s += self.map[r, c]
                 else:
                     s += self.map[r, c]
             print(s)
+
+    def score(self):
+        return sum(x.hit_points for x in self.units if x.alive()) * self.round_count
+
+    def round(self, verbose=False):
+        for u in self.units:
+            if u.alive():
+                success = u.play(self)
+                if not success:
+                    return True
+        self.units = [x for x in self.units if x.alive()]
+        self.units = sorted(self.units, key=lambda x: x.position)
+        if verbose:
+            self.print()
+            print(self.units)
+            print()
+        self.round_count += 1
+        return False
+
+    def round2(self, verbose=False):
+        for u in self.units:
+            if u.alive():
+                success = u.play(self)
+                if not success:
+                    return True
+            else:
+                if type(u).__name__ == "Elf":
+                    print("Elf died!")
+                    return True
+        self.units = [x for x in self.units if x.alive()]
+        self.units = sorted(self.units, key=lambda x: x.position)
+        if verbose:
+            self.print()
+            print(self.units)
+            print()
+        self.round_count += 1
+        return False
