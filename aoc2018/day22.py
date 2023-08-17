d9daf7fcecea0a7ae03b08149e9693f72814d1a6
Daniel Halligan <dan.halligan@gmail.com>
2023-08-17 16:36:35 +0100

    Day22 done

--- aoc2018/day22.py
+++ aoc2018/day22.py
@@ -1,6 +1,11 @@
 import re
-from functools import cache
+import sys
 
+from functools import cache
+from heapq import *
+
+
+sys.setrecursionlimit(10000)
 
 
 @cache
@@ -18,22 +23,29 @@ def geologic_index(x, y, t1, t2, depth):
     return a * b
 
 
+@cache
 def erosion_level(x, y, t1, t2, depth):
     return (geologic_index(x, y, t1, t2, depth) + depth) % 20183
 
 
-def shape(x):
-    if x == 0:
-        return "."
-    elif x == 1:
-        return "="
-    else:
-        return "|"
+@cache
+def terrain(pos):
+    return erosion_level(pos[0], pos[1], target[0], target[1], depth) % 3
+
+
+def moves(pos):
+    out = [
+        (pos[0], pos[1] + 1),
+        (pos[0], pos[1] - 1),
+        (pos[0] + 1, pos[1]),
+        (pos[0] - 1, pos[1]),
+    ]
+    return [x for x in out if x[0] >= 0 and x[1] >= 0]
 
 
 lines = open("inputs/day22.txt").read().splitlines()
 depth = int(re.findall("\d+", lines[0])[0])
-target = [int(x) for x in re.findall("\d+", lines[1])]
+target = tuple([int(x) for x in re.findall("\d+", lines[1])])
 
 # size = [16, 16]
 # cave = {}
@@ -42,10 +54,47 @@ target = [int(x) for x in re.findall("\d+", lines[1])]
 #         cave[x, y] = erosion_level(x, y, target[0], target[1], depth)
 
 # for y in range(size[0]):
-#     print("".join(shape(cave[x, y] % 3) for x in range(size[1])))
+#     print("".join([".", "=", "|"][cave[x, y] % 3] for x in range(size[1])))
 
-sum(
-    erosion_level(x, y, target[0], target[1], depth) % 3 
-    for x in range(target[0] + 1) 
-    for y in range(target[1] + 1)
-)
+
+def part1():
+    return sum(
+        terrain((x, y)) for x in range(target[0] + 1) for y in range(target[1] + 1)
+    )
+
+
+# Moving to an adjacent region takes one minute
+# You can change if your new equipment would be valid for your current region
+# Switching to using the climbing gear, torch, or neither always takes seven minutes
+# Once you reach the target, you need the torch equipped
+
+
+def part2():
+    # tool options
+    options = [set(["C", "T"]), set(["C", "-"]), set(["T", "-"])]
+
+    start = (0, 0)
+    queue = []  # queue items are (time, position, equipment)
+    heappush(queue, (0, start, "T"))
+    seen = {}
+    best = 5000
+
+    while queue:
+        time, pos, eq = heappop(queue)
+        if pos == target and eq == "T" and time < best:
+            best = time
+        if time > best or ((pos, eq) in seen and time >= seen[(pos, eq)]):
+            continue
+        seen[(pos, eq)] = time
+        tc = terrain(pos)
+
+        # We can move position (if tool is valid)
+        for npos in moves(pos):
+            if eq in options[terrain(npos)]:
+                heappush(queue, (time + 1, npos, eq))
+
+        # Or we can change tool if valid in our current terrain
+        for neq in options[tc].difference(eq):
+            heappush(queue, (time + 7, pos, neq))
+
+    return best
